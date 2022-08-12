4a36d20627d24e0cdb586d899a98073d8a582238
ChristinaMifsud <102205880+christina-mifsud@users.noreply.github.com>
2022-08-12 20:37:56 +0100

    added overlay & text

--- personal-home-page-template/src/script.js
+++ personal-home-page-template/src/script.js
@@ -1,16 +1,35 @@
+/* Load Overlay with Text */
 
-/************************************ */
-function on() {
+function overlay() {
   document.getElementById("overlay").style.display = "block";
+  document.getElementById("text").style.display = "block";
 }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+/* Flashing Cursor for Input */
 
-/************************************ */
+let cursor = true;
+let speed = 250;
+setInterval(() => {
+  if (cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+  } else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+  }
+}, speed);
+
+/* Turn Off On Click */
+
+//window.onclick = off;
+
+// function off() {
+//   document.getElementById("overlay").style.display = "none";
+// };
 
 
+
+/* Input to Speech Feature */
 
 function getNumberOrString(value) {
   // Convert a string value to a number if possible
@@ -22,12 +41,10 @@ function getNumberOrString(value) {
   }
 }
 
-
-
 document.getElementById('button').addEventListener('click', (event) => {
   window.speechSynthesis.speak(new SpeechSynthesisUtterance('I see you '));
   window.speechSynthesis.speak(new SpeechSynthesisUtterance(getNumberOrString(document.getElementById('name').value)));
-
+  document.getElementById("overlay").style.display = "none";
 });
 
 
